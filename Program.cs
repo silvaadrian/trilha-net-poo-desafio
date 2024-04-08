3aabf8f556c371edcc65d2304d096a520bd1427a
Adrian <164921227+silvaadrian@users.noreply.github.com>
2024-04-08 13:14:07 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone i = new Iphone("997886543","15","8237237AV",50);
+Nokia n = new Nokia("998765438", "A11", "234332432gs", 60);
+i.Apresentar();
+i.InstalarAplicativo("Orkut");
+n.Apresentar();
+n.InstalarAplicativo("Orkut");
